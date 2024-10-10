ba20123c2759f9d6c79555739cd1b4ced86d341d
blackBuzo2601 <elianpro58@gmail.com>
2024-10-10 00:36:03 -0700

    Completed logic to draw the chart using the json file

--- versionWeb/view/app.js
+++ versionWeb/view/app.js
@@ -1,32 +1,35 @@
 const ctx = document.getElementById('myChart').getContext('2d');
 
 //Cargamos el JSON
-fetch('prueba.json')
+fetch('data.json')
     .then(response => response.json())
     .then(dataParseada => { //almacenamos la data para poder manipularla.
         
         const etiquetasChart = []; //variable que se usará para guardar las etiquetas pertenecientes a los días (el eje X)
-
+        const datosChart=[];
         /*Guardamos las fechas en un arreglo para utilizar dicho array para generar los labels
         del chart*/
-        for(let i=0;i<dataParseada.data.length;i++){ //iterar sobre cada objeto en data
-            etiquetasChart.push(dataParseada.data[i].date);
+       
+        
+        for(let i=0;i<dataParseada.length;i++){ //iterar sobre cada objeto en data
+            etiquetasChart.push(dataParseada[i].fecha_hora);
+            datosChart.push(dataParseada[i].temperatura);
         }
+        //como guardamos al mismo tiempo en cada array tanto el dato como la fecha y hora, siempre coincidirá
 
-        for(let i=0;i<dataParseada.data.length;i++){ //iterar sobre cada objeto en data
-           console.log(dataParseada.data[i].records); //obtenemos los datos del dia correspondiente.
-        }
+        //console.log(dataParseada[1]); //asi vemos el objeto, es decir, la captura completa del dato
+        //con todo y fecha.
 
         const myChart = new Chart(ctx, {
             type: 'line',
             data: {
                 labels: etiquetasChart,
                 datasets: [{
-                    label: 'TEMPERATURA EN CELSIUS',
-                    data: [1,2,3],
+                    label: 'TEMPERATURA EN °C',
+                    data: datosChart,
                     backgroundColor: 'rgba(76, 191, 122, 0.2)',
                     borderColor: 'rgba(48, 162, 93, 1)',
-                    borderWidth: 8,
+                    borderWidth: 5,
                     fill: false,
                 }]
             },
@@ -34,10 +37,10 @@ fetch('prueba.json')
                 responsive: false,  
                 scales: {
                     x: {
-                        beginAtZero: true,
+                        beginAtZero: false,
                     },
                     y: {
-                        beginAtZero: true
+                        beginAtZero: false
                     }
                 }
             }
@@ -45,7 +48,3 @@ fetch('prueba.json')
     })
 .catch(error => console.error('Error al cargar los datos:', error));
 
-//Estructura del JSON para que no se me olvide como acceder a los datos xd
-//console.log(dataParseada.data[0]);   //accediendo al objeto del dia 1
-//console.log(dataParseada.data[0].date); //obteniendo fecha para etiquetas (chart)
-//console.log(dataParseada.data[2].records); //obteniendo todos los datos del dia 1.
